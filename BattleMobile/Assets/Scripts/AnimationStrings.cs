4d46bf0f8acec9c15f0041da0058d1d692467200
lollo-ui <124943767+lollo-ui@users.noreply.github.com>
2023-03-01 23:24:43 +0100

    new script and falling animation

--- BattleMobile/Assets/Scripts/AnimationStrings.cs
+++ BattleMobile/Assets/Scripts/AnimationStrings.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
     internal static string isMoving = "isMoving";
     internal static string isRunning = "isRunning";
+    internal static string isGrounded = "isGrounded";
 }
 
 
