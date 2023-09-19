b9a0c846b0c5977918c622f2535f23fc7eaa1fcc
lollo-ui <lor.bianco2@stud.uniroma3.it>
2023-09-19 17:47:19 +0200

    Damiano : setUp playFab

---
+++ BattleMobile/Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayFabManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Success login");
+    }
+
+    void OnError(PlayFabError errror)
+    {
+        Debug.Log("Error while logging in");
+        Debug.Log(errror.GenerateErrorReport());
+    }
+}
